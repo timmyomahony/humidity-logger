8d0c0735dd0a240e254a6c8801c63d3e8db31ff2
Timmy O'Mahony <hey@timmyomahony.com>
2020-07-19 09:19:23 +0100

    Added scripts

---
+++ humidity_logger.py
@@ -0,0 +1,22 @@
+# https://pimylifeup.com/raspberry-pi-humidity-sensor-dht22/
+
+import os
+import time
+import Adafruit_DHT
+
+DHT_SENSOR = Adafruit_DHT.DHT22
+DHT_PIN = 4
+
+try:
+    f = open('/home/pi/humidity.csv', 'a+')
+    if os.stat('/home/pi/humidity.csv').st_size == 0:
+        f.write('Date,Time,Temperature,Humidity\r\n')
+except:
+        pass
+
+humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+
+if humidity is not None and temperature is not None:
+    f.write('{0},{1:0.1f},{2:0.1f}\r\n'.format(time.strftime('%m/%d/%y %H:%M:%S'), temperature, humidity))
+else:
+    print("Failed to retrieve data from humidity sensor")
