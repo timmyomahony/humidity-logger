52466a7f2e1694b19f41a46fb0102840a5333ffb
Timmy O'Mahony <hey@timmyomahony.com>
2020-07-19 09:50:40 +0100

    Added graphing

---
+++ create_graph.py
@@ -0,0 +1,24 @@
+import csv
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+dates = []
+temperatures = []
+humidities = []
+
+with open("/home/pi/humidity.csv") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    next(csv_file)
+    for row in csv_reader:
+        dates.append(row[0])
+        temperatures.append(row[1])
+        humidities.append(row[2])
+
+dates = [datetime.strptime(date, "%m/%d/%y %H:%M:%S") for date in dates]
+
+plt.plot(dates, temperatures, color="blue", marker="o")
+plt.title("Temperature", fontsize=12)
+plt.xlabel("Date", fontsize=12)
+plt.ylabel("Temperature", fontsize=12)
+plt.grid(True)
+plt.savefig("/home/pi/output/temperature-{}.png".format(datetime.now()))
